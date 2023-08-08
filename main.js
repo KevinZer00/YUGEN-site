61e81cd7144bd73a7c6ace39dcbdfbaecfa2f45a
Kevin Yu <kevinyuzer0@gmail.com>
2023-08-08 02:10:44 -0700

    Fixed imports

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/OrbitControls.js';
+import { GLTFLoader } from 'three/GLTFLoader.js';
 
 particlesJS.load('particles-js', 'particlesjs-config.json',
 function() {
